bbec3d34a1c1d8f40b9c7022d5df7eb0c42ca628
san99tiago <san99tiago@gmail.com>
2020-05-04 01:55:46 -0500

    Way to <import> stuff from other JS files

---
+++ PROJECT_0/index2.js
@@ -0,0 +1,35 @@
+//ACCESSING OTHER JS FILE AND ITS DIFFERENT APPROACHES
+//Santiago Garcia Arango, May 2020
+
+//When obtaining other file using "exports" way, then we must understand that...
+//...we can "call" the other file to execute directly, or to strategically access...
+//...some atributtes or functions from it.
+
+
+
+//----------------------ACQUIRING THE OTHER FILE TO DIRECTLY EXECUTE IT-------------------
+//this is how we get access to "execute" a file specified (relative path)
+require('./index.js');
+console.log("\nDONE EXECUTING FIRST FILE!!\n");
+//However, this approach is NOT the best, the next way gives us more control over things
+
+
+
+
+//----------------------ACQUIRING THE OTHER FILE TO EXECUTE WHAT WE WANT-------------------
+//Now we show the "exports" from the first file (but with more control over them)
+console.log("NOW WE ACCESS TO ITS <EXPORTS> TO SHOW THEM STRATEGICALLY:");
+
+//We create a variable called "info" to acquire it exported elements
+const info = require('./index.js');
+console.log(info.first_thing);
+console.log(info.second_thing);
+
+
+//This is how we can actually run the "exports" function created in "index.js"
+info.MySuperFunction();
+
+
+//Finally, this is how the whole "exports" obtained from the "index.js" file looks like:
+console.log("\nTHIS IS HOW THE EXPORTS OBTAINES LOOK LIKE:");
+console.log(info);
