16763e7a16c87001323e82e89c6c8da2bb9457f0
san99tiago <san99tiago@gmail.com>
2020-05-04 03:16:34 -0500

    example of import modules globally

---
+++ PROJECTS/PROJECT_1/m3.js
@@ -0,0 +1,11 @@
+//SIMPLE SCRIPT TO IMPORT A GLOBAL COMMON MODULE
+//Santiago Garcia Arango, May 2020
+
+//We now have access to the "http" module (which is global in NODEJS)
+import http from 'http';
+
+//We explore what it has (methods , STATUS_CODES , createServer, get, request, etc...)
+console.log(http);
+
+
+
