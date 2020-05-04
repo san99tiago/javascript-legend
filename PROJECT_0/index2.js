031714a4f9872369989db03cef96eb06bd640f50
san99tiago <san99tiago@gmail.com>
2020-05-04 02:04:30 -0500

    minor changes in spelling

--- PROJECT_0/index2.js
+++ PROJECT_0/index2.js
@@ -9,7 +9,7 @@
 
 //----------------------ACQUIRING THE OTHER FILE TO DIRECTLY EXECUTE IT-------------------
 //this is how we get access to "execute" a file specified (relative path)
-require('./index.js');
+require('./index1.js');
 console.log("\nDONE EXECUTING FIRST FILE!!\n");
 //However, this approach is NOT the best, the next way gives us more control over things
 
@@ -18,18 +18,18 @@ console.log("\nDONE EXECUTING FIRST FILE!!\n");
 
 //----------------------ACQUIRING THE OTHER FILE TO EXECUTE WHAT WE WANT-------------------
 //Now we show the "exports" from the first file (but with more control over them)
-console.log("NOW WE ACCESS TO ITS <EXPORTS> TO SHOW THEM STRATEGICALLY:");
+console.log("NOW WE ACCESS TO INDEX1.JS <EXPORTS> TO SHOW THEM:");
 
 //We create a variable called "info" to acquire it exported elements
-const info = require('./index.js');
+const info = require('./index1.js');
 console.log(info.first_thing);
 console.log(info.second_thing);
 
 
-//This is how we can actually run the "exports" function created in "index.js"
+//This is how we can actually run the "exports" function created in "index1.js"
 info.MySuperFunction();
 
 
-//Finally, this is how the whole "exports" obtained from the "index.js" file looks like:
+//Finally, this is how the whole "exports" obtained from the "index1.js" file looks like:
 console.log("\nTHIS IS HOW THE EXPORTS OBTAINED LOOK LIKE:");
 console.log(info);
