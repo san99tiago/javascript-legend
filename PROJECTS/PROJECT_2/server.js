d08119cc552560fe6bff77dec800c2c90733d495
san99tiago <san99tiago@gmail.com>
2020-05-10 03:29:54 -0500

    add the start of the service

--- PROJECTS/PROJECT_2/server.js
+++ PROJECTS/PROJECT_2/server.js
@@ -1,3 +1,20 @@
 // WERE ARE GOING TO CREATE OUR FIRST SERVER HERE
 
 console.log("WE ARE GOING TO CREATE OUR FIRST SERVER HERE!!!\n");
+
+
+//We import the functionality of "http" (from the "node.js" module)
+import http from 'http'
+
+//Create a new server (this is from the "node.js" module imported before)
+const server = http.createServer();
+
+
+
+
+
+
+
+//By this command, we keep the server running (otherways, it will stop)
+server.listen( 3000 );
+
