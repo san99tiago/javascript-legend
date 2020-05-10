21ad10d0875bda0b14b48a7f6237dbb5cdacbdc6
san99tiago <san99tiago@gmail.com>
2020-05-10 04:08:09 -0500

    new server with express.js

--- PROJECTS/PROJECT_2/server.js
+++ PROJECTS/PROJECT_2/server.js
@@ -1,13 +1,25 @@
 // WERE ARE GOING TO CREATE OUR FIRST SERVER HERE
+//Santiago Garcia Arango, May 2020
 
 console.log("WE ARE GOING TO CREATE OUR FIRST SERVER HERE!!!\n");
 
+//This is how we import the express module, in order to create servers easily
+import express from 'express'
 
-//We import the functionality of "http" (from the "node.js" module)
-import http from 'http'
+//This is the default function to create and initialize a server with express!
+const server = express();
 
-//Create a new server (this is from the "node.js" module imported before)
-const server = http.createServer();
+
+//Handle GET events on the server and make a "response" 
+//It is important to notice that the route is '/' (after the IP address and port)
+server.get('/', (request,response) => {
+    //We make a response with a basic message (and the correct status code (200))
+    response.send({message: "HELLO FRIENDS, I'M WORKING WITH EXPRESS!"});
+    response.status(200);
+
+    //We show that a GET was donde (in the terminal)
+    console.log("get donde to '/' ");
+})
 
 
 
@@ -16,5 +28,5 @@ const server = http.createServer();
 
 
 //By this command, we keep the server running (otherways, it will stop)
+//This must be always run at the end of the code
 server.listen( 3000 );
-
