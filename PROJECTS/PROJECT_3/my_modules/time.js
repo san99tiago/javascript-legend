80c72c2d22c290dc59e78c9c8b8d1724422d704e
san99tiago <san99tiago@gmail.com>
2020-05-13 04:15:34 -0500

    added new function to module

--- PROJECTS/PROJECT_3/my_modules/time.js
+++ PROJECTS/PROJECT_3/my_modules/time.js
@@ -9,8 +9,22 @@ function my_date() {
     var date = today.getDate()+'/'+today.getMonth()+ '/' + today.getFullYear();
     return date ;
 }
-//Then we export the desired functions with the "export" command
-exports.my_date = my_date;
+
+//Function that returns the specific time at this moment in the desired string format
+function my_time() {
+    var actual_time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    return actual_time;
+}
 
 
+
+
+//In order to export an object (instead of each function), this is how we do it
+const time = {}; //Create object to export
+time.my_date = my_date; //Add function to the object
+time.my_time = my_time; //Add function to the object
+
+
+//Then we export the whole object created before
+module.exports = time;
 
