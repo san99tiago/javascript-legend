c9febf2b354dec369799657753c42608292b5b0b
san99tiago <san99tiago@gmail.com>
2020-05-18 02:09:29 -0500

    simple module for handling system files from nodejs

--- PROJECTS/PROJECT_3/my_modules/create_txt.js
+++ PROJECTS/PROJECT_3/my_modules/create_txt.js
@@ -9,27 +9,27 @@ const fs = require("fs");
 //Function that allows us to create a file in the actual directory if it doesn't exists yet
 //(First parameter: name of the file that we create)
 //(Second parameter: data to write)
-//(Third parameter: callback)
+//(Third parameter: callback-->function that "receives" error when ending file operation)
 function create_file() {
-    fs.writeFile("./servers_inits/my_cool_info.txt", null, function() {
+    fs.writeFile("./servers_inits/my_cool_info.txt", null, function(err) {
+        
+        //This is how we check if there were errors in the "callback"
+        if (err){
+            console.log(err);
+        }
+
+        //If everything was ok (NO errors)
+        console.log("TXT file process was ok!!!");
+
 
     });
 }
-
-//Function that returns the specific time at this moment in the desired string format
-function my_time() {
-    var actual_time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return actual_time;
-}
-
 
 
 
 //In order to export an object (instead of each function), this is how we do it
 const fs = {}; //Create object to export
-fs.my_date = my_date; //Add function to the object
-time.my_time = my_time; //Add function to the object
-
+fs.create_file = create_file; //Add function to the object
 
 //Then we export the whole object created before
-module.exports = time;
+module.exports = fs;
