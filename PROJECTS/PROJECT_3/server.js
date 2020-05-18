f94db0172ba512a8d0ffee18ce6f1acb97f28534
san99tiago <san99tiago@gmail.com>
2020-05-18 02:08:59 -0500

    added another <my_modules> functionallity

--- PROJECTS/PROJECT_3/server.js
+++ PROJECTS/PROJECT_3/server.js
@@ -3,6 +3,7 @@
 
 //require my own modules (look at it in directory "my_modules")
 const time = require('./my_modules/time');
+const fs = require('./my_modules/create_txt');
 
 //require the "express.js" package and initialize it correctly in "app"
 const express = require('express')
@@ -15,7 +16,7 @@ app.get('/', (request,response) => {
     response.send("THIS IS MY FIRST RESPONSE!!!");
     response.status(200);
 
-    //We show that a GET was donde (in the terminal)
+    //We show that a GET was done (in the terminal)
     console.log("GET done to: '/'");
 })
 
@@ -26,10 +27,11 @@ app.get('/', (request,response) => {
 
 
 
-//By this command, we keep the server running (otherways, it will stop)
+// By this command, we keep the server running (otherwise, it will stop)
 //This must be always run at the end of the code
 //It is important to notice that the "function(){}" is a callback and helps us send info to terminal
 const server = app.listen( 3000 , function() {
     console.log('Listening on http://localhost:' + server.address().port );
     console.log( `${time.my_date()} , ${time.my_time()} , server initialized`);
+    fs.create_file();
 });
