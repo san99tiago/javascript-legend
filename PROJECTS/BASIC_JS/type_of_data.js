b183439091cee7cfc6e3902dd9c3ffb062f15244
Santiago <santiago.garcia1999@hotmail.com>
2020-12-17 21:43:44 -0500

    Added type_of_data for basic JS datatypes

---
+++ PROJECTS/BASIC_JS/type_of_data.js
@@ -0,0 +1,39 @@
+// TYPES OF VARIABLES IN JAVASACRIPT
+// Santiago Garcia Arango
+
+// Numbers
+console.log("\n---------NUMBERS---------")
+console.log(40)
+console.log("typeof(40): ", typeof(40))
+
+// Strings
+console.log("\n---------STRINGS---------")
+console.log("Santiago Garcia")
+console.log("typeof(\"Santiago Garcia\"): ", typeof("Santiago Garcia"))
+
+// Booleans
+console.log("\n---------BOOLEANS---------")
+console.log(true)
+console.log(false)
+console.log("typeof(true):", typeof(true))
+console.log("typeof(false):", typeof(false))
+
+// Null
+console.log("\n----------NULL-----------")
+console.log(null)
+console.log("typeof(null):", typeof(null))
+
+// Undefined **(should avoid this type of variables)
+console.log("\n--------UNDEFINED---------")
+console.log(undefined)
+console.log("typeof(undefined):", typeof(undefined))
+
+// Arrays
+console.log("\n---------ARRAYS----------")
+console.log([1, 1, 2, 3, 5])
+console.log("typeof([1, 1, 2, 3, 5]):", typeof([1, 1, 2, 3, 5]))
+
+// Objects
+console.log("\n---------OBJECTS----------")
+console.log({name: "Santiago"})
+console.log("typeof({name: \"Santiago\"}):", typeof({name: "Santiago"}))
