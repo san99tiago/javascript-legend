44cdd61e877e5e7792adee07ad89d220c79e926c
Santiago <santiago.garcia1999@hotmail.com>
2020-12-19 03:15:51 -0500

    Included while loop in <loops.js> file

--- PROJECTS/BASIC_JS/loops.js
+++ PROJECTS/BASIC_JS/loops.js
@@ -2,7 +2,7 @@
 // Santiago Garcia Arango
 
 var my_tools = ["Python", "JavaScript", "Java", "C", "C++", "PHP", "MySQL",
-    "Arduino", "LabVIEW", "HTML5", "CSS3", "Sass", "TypeScript"];
+    "Arduino", "LabVIEW", "HTML5", "CSS3"];
 
 function share_my_tool(tool, tool_number) {
     console.log("My tool", String(tool_number), "is :", tool);
@@ -16,8 +16,16 @@ for (let i = 0; i < my_tools.length; i++) {
 
 // Special "for of" loop
 console.log("\n------------SECOND FOR LOOP-------------")
-var i = 0;
+var tool_number = 0;
 for (var tool of my_tools) {
-    share_my_tool(tool, i);
+    share_my_tool(tool, tool_number);
+    tool_number++;
+}
+
+// Generic "while" loop
+console.log("\n------------WHILE LOOP-------------")
+var i = 0;
+while (i < my_tools.length) {
+    share_my_tool(my_tools[i], i);
     i++;
 }
