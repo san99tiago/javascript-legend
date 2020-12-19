0747bb68e9eb55e945ee8bcb3ce1509cdc08a5be
Santiago <santiago.garcia1999@hotmail.com>
2020-12-19 01:57:46 -0500

    Added Boolean analysis examples to BASIC_JS

---
+++ PROJECTS/BASIC_JS/boolean_analysis.js
@@ -0,0 +1,20 @@
+// BOOLEAN ANALYSIS IN JAVASACRIPT
+// Santiago Garcia Arango
+
+// Negative (false) Booleans
+console.log("\n---------FALSE BOOLEANS---------")
+console.log(Boolean(0))
+console.log(Boolean(null))
+console.log(Boolean(NaN))
+console.log(Boolean(undefined))
+console.log(Boolean(false))
+console.log(Boolean(""))
+
+// Positive (true) Booleans
+console.log("\n---------TRUE BOOLEANS---------")
+console.log(Boolean(1))
+console.log(Boolean(-1))
+console.log(Boolean("a"))
+console.log(Boolean([]))
+console.log(Boolean({}))
+console.log(Boolean(function() {}))
