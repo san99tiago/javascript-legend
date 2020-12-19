c561c60abab7c12f4d6b13b2f87d1ae870849d64
Santiago <santiago.garcia1999@hotmail.com>
2020-12-19 02:18:52 -0500

    Added conditionals for JavaScript basics

---
+++ PROJECTS/BASIC_JS/conditionals.js
@@ -0,0 +1,28 @@
+// CONDITIONALS IN JAVASACRIPT
+// Santiago Garcia Arango
+
+// If statement
+console.log("\n---------EXAMPLE 1-----------");
+if (true) {
+    console.log("Entered if statement");
+}
+
+// If else statement
+console.log("\n---------EXAMPLE 2-----------");
+if (false) {
+    console.log("Entered if statement");
+} else {
+    console.log("Entered the else statement");
+}
+
+// If else-if statement
+console.log("\n---------EXAMPLE 3-----------");
+if (false) {
+    console.log("Entered if statement");
+} else if (false) {
+    console.log("Entered the first else-if statement");
+} else if (true) {
+    console.log("Entered the second else-if statement");
+} else {
+    console.log("Entered the else statement");
+}
