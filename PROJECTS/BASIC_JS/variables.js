59cc27a83f372fcddb33d62d65c93ccbc8266309
Santiago <santiago.garcia1999@hotmail.com>
2020-12-18 19:14:36 -0500

    Added variables.js to basic_js scripts

---
+++ PROJECTS/BASIC_JS/variables.js
@@ -0,0 +1,33 @@
+// VARIABLES IN JAVASACRIPT
+// Santiago Garcia Arango
+
+// Declare variables (only initialized)
+var user_id;
+
+// Add data to those initialized variables
+var user_id = "111111111";
+
+// Create variables with specific values given
+var user_name = "Santiago";
+var user_lastname = "Garcia";
+var user_age = 21;
+
+// Create variable of "array" type
+var information_array = [user_name, user_lastname, user_age, user_id]
+
+// Create variables of "object" type
+var information_object = {
+    user_name: user_name,
+    user_lastname: user_lastname,
+    user_age: user_age,
+    user_id: user_id
+}
+
+// Show variables
+console.log("\n----------------SHOW OUR VARIABLES----------------\n")
+console.log("user_name: ", user_name, "\n")
+console.log("user_lastname: ", user_lastname, "\n")
+console.log("user_age: ", user_age, "\n")
+console.log("user_id: ", user_id, "\n")
+console.log("information_array: ", information_array, "\n")
+console.log("information_object: ", information_object, "\n")
