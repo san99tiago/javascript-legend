452ffea4eeb96a934a6a80cf3e4f7310d39a346d
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-03-29 16:43:11 -0500

    Linted async-projects and added requests to asyn_js

--- PROJECTS/ASYNC_JS/src/index.js
+++ PROJECTS/ASYNC_JS/src/index.js
@@ -3,31 +3,28 @@
 
 // Main function
 function sum(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 // Simple callback structure for a general function
 function calc(num1, num2, callback) {
-    return callback(num1, num2);
+  return callback(num1, num2);
 }
 
 // Function to understand callbacks with dates and other printDate function
 function myDate(callback) {
-    console.log('FIRST LOG: ', new Date);
-    setTimeout(function () {
-        let date = new Date;
-        callback(date);
-    }, 3000)
+  console.log("FIRST LOG: ", new Date());
+  setTimeout(function () {
+    let date = new Date();
+    callback(date);
+  }, 3000);
 }
 
 // Simple function to apply as a callback in <myDate>
 function printDate(dateNow) {
-    console.log('PRINT DATE FUNCTION: ', dateNow);
+  console.log("PRINT DATE FUNCTION: ", dateNow);
 }
 
 // TESTS
 console.log(calc(8, 4, sum));
 myDate(printDate);
-
-
-
